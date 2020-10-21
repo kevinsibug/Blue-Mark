5468c9bc08c64f6f0c0a1934184e51e31fd2b0ea
kedansi <38392797+kedansi@users.noreply.github.com>
2020-10-21 19:55:07 +0800

    Added Customer and Package models

--- delivery_tracker/models.py
+++ delivery_tracker/models.py
@@ -8,7 +8,14 @@ class Customer(models.Model):
     address = models.TextField()
     phone = models.CharField(max_length=11)
 
+    def __str__(self):
+        return self.name
+
 class Package(models.Model):
 
-    Package_type = models.TextField()
-    Package_weight= models.IntegerField()
+    package_type = models.TextField()
+    package_weight= models.IntegerField()
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.package_type
--- delivery_tracker/views.py
+++ delivery_tracker/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from delivery_tracker.models import *
+
+
 # Create your views here.
+def package_detail(request, pk):
+
+    customer_obj = Driver.objects.get(pk=pk)
+
+    car_objs = Car.objects.filter(owner_id=owner_obj.id)
+
+    context = {
+
+        "vehicles": car_objs,
+
+        "drivers": owner_obj,
+
+    }
+
+    return render(request, "car_detail.html", context)
