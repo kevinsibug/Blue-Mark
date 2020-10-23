2af85136e7d70790665a7c84f498fd0a86f75b7e
kedansi <38392797+kedansi@users.noreply.github.com>
2020-10-23 19:02:20 +0800

    Edited templates - Added base.html

--- delivery_tracker/urls.py
+++ delivery_tracker/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('addcustomer/', views.add_customer, name='add_customer'),
     path('viewcustomers/', views.view_customers, name='view_customers'),
-    path('<int:pk>/', views.customer_detail, name="customer_detail"),
+    path('customers/<int:pk>/', views.customer_detail, name="customer_detail"),
     path('trackpackages/', views.packages_list, name='packages_list'),
 ]
