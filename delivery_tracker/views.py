fae829a04d7e454c8e5cde3ae18221034add481a
kedansi <38392797+kedansi@users.noreply.github.com>
2020-10-24 10:12:46 +0800

    Edited Templates, views, and models

--- delivery_tracker/views.py
+++ delivery_tracker/views.py
@@ -17,13 +17,19 @@ def customer_detail(request, pk):
 
     customer_obj = Customer.objects.get(pk=pk)
 
-    package_objs = Package.objects.filter(customer_id=customer_obj.id)
+    # package_objs = Package.objects.filter(customer_id=customer_obj.id)
+
+    # Trial
+
+    request_objs = Delivery_Request.objects.filter(customer_id=customer_obj.id)
 
     context = {
 
-        "packages": package_objs,
+        # "packages": package_objs,
 
         "customers": customer_obj,
+
+        "requests": request_objs,
 
     }
     return render(request, "customer_detail.html", context)
