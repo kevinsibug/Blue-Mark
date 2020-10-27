c3d51c26895ba5879451613a5721f0f70763a5b8
Kyla Nicole Villegas <kyla.villegas@obf.ateneo.edu>
2020-10-27 12:18:53 +0800

    Fixed createdeliveryrequest/customerconfirmation/ in urls.py and createdeliveryrequest/customerconfirmation/.html

--- delivery_tracker/urls.py
+++ delivery_tracker/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     path('', views.home, name="home"),
     path('createdeliveryrequest/', views.create_delivery_request, name='create_delivery_request'),
-    path('addcustomer/customerconfirmation/', views.customer_confirmation, name='customerconfirmation'),
+    path('createdeliveryrequest/customerconfirmation/', views.customer_confirmation, name='customerconfirmation'),
     path('viewcustomers/', views.view_customers, name='view_customers'),
     path('customers/<int:pk>/', views.customer_detail, name='customer_detail'),
     path('trackpackages/', views.packages_list, name='packages_list'),
